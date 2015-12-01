f12807ec056d21a52c5d5d65a6d3a2641550df69
Erik Öhrn <erik.ohrn@outlook.com>
2015-12-01 09:50:10 +0100

    Commented code

--- Lab4/queue.c
+++ Lab4/queue.c
@@ -4,6 +4,9 @@
 /* Implementera interface från queue.h här */
 QueuePtr head;
 
+/*
+ * New queue. Skapar en ny kö, det vill säga läser in startvärden till köns HEAD.
+ */
 QueuePtr new_queue() {
     head->prio = MAX_PRIO;
     head->data = 0;

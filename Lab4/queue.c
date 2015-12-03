50b461f039f989fc02669c134850660bf43ac1de
Erik Öhrn <erik.ohrn@outlook.com>
2015-12-03 09:51:36 +0100

    Added a version of add

--- Lab4/queue.c
+++ Lab4/queue.c
@@ -15,8 +15,30 @@ QueuePtr new_queue() {
 };
 
 /*
+ * Inserts an element after another
+ */
+void insert(QueuePtr q_before, QueuePtr q_after){
+    q_after->next=q_before->next;
+    q_before->next=q_after;
+}
+
+/*
  * Adds a new element to the queue
  */
-void add(QueuePtr q, int prio, DataPtr d){
- QueuePtr new_element = malloc(sizeof new_element);
+void add(QueuePtr q, int prio, DataPtr d) {
+    QueuePtr new_element = malloc(sizeof new_element);
+    new_element->next = q;
+    new_element->prio = prio;
+    new_element->data = d;
+    QueuePtr other = head;
+    while (1) {
+        if (other->prio >= prio && other->next != NULL) {
+            other = other->next;
+        } else {
+            insert(other, new_element);
+            return;
+        }
+    }
+
 };
+

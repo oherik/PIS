2d9101175aa3e46a49c99aaa31f6c5ae098e3222
Erik Öhrn <erik.ohrn@outlook.com>
2015-12-04 13:34:50 +0100

    Added some kind of fance documentation

--- Lab4/queue.c
+++ Lab4/queue.c
@@ -1,8 +1,9 @@
 #include "queue.h"
 #include <stdlib.h>
 
-/*
- * New queue. Creates a new queue with a head element. Returns the head pointer.  
+/**
+ * @brief New queue. Creates a new queue with a head element. Returns the head pointer.  
+ * @return  The head of the new queue
  */
 QueuePtr new_queue() {
     QueuePtr head =  malloc(sizeof head);   // Allocates memory for the head
@@ -10,16 +11,21 @@ QueuePtr new_queue() {
     return head;                      
 }
 
-/*
- * Inserts an element after another
+/**
+ * @brief Inserts an element after another
+ * @param q_before  The first element
+ * @param q_after   The element which is to be placed after the first
  */
 void insert(QueuePtr q_before, QueuePtr q_after){
     q_after->next = q_before->next;
     q_before->next =q_after;
 } 
 
-/*
- * Adds a new element to the queue based on the priority. R
+/**
+ * @brief Adds a new element to the queue based on the priority. 
+ * @param   q   The head of the queue
+ * @param   prio    The priority of the element
+ * @param   d   The data pointer of the element
  */
 void add(QueuePtr q, int prio, DataPtr d) {
     QueuePtr new_element = malloc(sizeof new_element);  // Allocates memory for the new element
@@ -31,8 +37,10 @@ void add(QueuePtr q, int prio, DataPtr d) {
     insert(q, new_element);                         // Either the list has ended or an element with lower priority has been found. Insert the new element after the other.    
 }
 
-/*
- * Returns the first data value. q is the head of the queue.
+/**
+ * @brief Returns the first data value. q is the head of the queue.
+ * @param  q    The head of the queue
+ * @return  The first data element of the queue, if it exists, NULL otherwise
  */
 DataPtr get_first(QueuePtr q){
     if(!q->next){
@@ -42,8 +50,9 @@ DataPtr get_first(QueuePtr q){
     }
 }
 
-/*
- * Returns the length of the queue. q is the head of the queue.
+/**
+ * @brief Returns the length of the queue. 
+ * @param  q    The head of the queue
  */
 int size(QueuePtr q){
     int length = 0;
@@ -53,4 +62,10 @@ int size(QueuePtr q){
     }
     return length;
 }
+
+/**
+ * @brief
+ * @param q
+ */
+
 

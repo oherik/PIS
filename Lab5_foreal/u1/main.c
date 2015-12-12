8b5387e6ab46e16865db02e720040e63ba9ce7bd
Erik Öhrn <erik.ohrn@outlook.com>
2015-12-12 17:27:08 +0100

    Added 5.1

--- Lab5_foreal/u1/main.c
+++ Lab5_foreal/u1/main.c
@@ -1,7 +1,16 @@
+#include "ports.h"
+
 #define	set(x, mask)	(x) = (x) | (mask)
 #define clear(x, mask)	(x) = (x) & ~(mask)
 
-unsigned char	ML4shadow = 0;
+unsigned char ML4shadow = 0;
 
 void main(void){
+	int value;
+	while(0){
+		value = ML4IN;
+		ML4shadow = value;
+		ML4OUT = value;
+	}
 }
+

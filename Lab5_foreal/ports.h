14e6899c6ea34d440ab1a9796e8662a43f14be31
Erik Öhrn <erik.ohrn@outlook.com>
2015-12-12 16:12:30 +0100

    Fixed ports.h some more

--- Lab5_foreal/ports.h
+++ Lab5_foreal/ports.h
@@ -1,7 +1,7 @@
-#define	ML4IN	0x600
-#define	ML4OUT	0x400
+#define	ML4IN_ADDRESS	0x600
+#define	ML4OUT_ADDRESS	0x400
 
 typdef	unsigned char * port8ptr;
 
-#define	ML4READ	*((port8ptr) ML4IN)
-#define	ML4WRITE *((port8ptr) ML4OUT)
+#define	ML4IN	*((port8ptr) ML4IN_ADDRESS)
+#define	ML4OUT*((port8ptr) ML4OUT_ADDRESS)

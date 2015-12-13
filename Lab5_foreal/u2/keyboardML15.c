532c045cbed2a3d1231aee32abc4e977c79a7719
Erik Öhrn <erik.ohrn@outlook.com>
2015-12-13 13:10:05 +0100

    minor fixes

--- Lab5_foreal/u2/keyboardML15.c
+++ Lab5_foreal/u2/keyboardML15.c
@@ -3,10 +3,15 @@
 #define clear(x, mask)	(x) &= ~(mask)
 
 char get_key(void){
+	char tmp;
 	char key = ML15IN;		//Läs in fråm ML2
-	while(key<=0){			//key kommer vara >0 när en tangent finns tillgänglig
+	while(key<0){			//key kommer vara >0 när en tangent finns tillgänglig
 		key = ML15IN;	
 	}
+	tmp = ML15IN;
+	while(tmp>=0){
+		tmp = ML15IN;
+	}				//Busy wait tills personen släppt knappen
 	clear(key, 0xF0);		//Tangenten ligger i bit 0-3
 	return key;	
 }
--- Lab5_foreal/u2/main.c
+++ Lab5_foreal/u2/main.c
@@ -1,5 +1,7 @@
 
 void main(void){
+while(1){
 	char key = get_key();
 	display_dec(key);
+	}
 }

96af76f06735ecf8f62f1bcc3dfba95eba86fe22
Erik Öhrn <erik.ohrn@outlook.com>
2015-12-13 09:40:30 +0100

    Added frame for u2

--- Lab5_foreal/ports.h
+++ Lab5_foreal/ports.h
@@ -1,7 +1,7 @@
 #define	ML4IN_ADDRESS	0x600
 #define	ML4OUT_ADDRESS	0x400
 
-typedef	unsigned char * port8ptr
+typdef	unsigned char * port8ptr;
 
 #define	ML4IN	*((port8ptr) ML4IN_ADDRESS)
-#define	ML4OUT	*((port8ptr) ML4OUT_ADDRESS)
+#define	ML4OUT*((port8ptr) ML4OUT_ADDRESS)

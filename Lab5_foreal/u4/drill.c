efdebdc6cdfec80a5150f308d0984ff2c2821f6c
Erik Öhrn <erik.ohrn@outlook.com>
2015-12-15 09:14:09 +0100

    Added outone and outzero

--- Lab5_foreal/u4/drill.c
+++ Lab5_foreal/u4/drill.c
@@ -1,2 +1,39 @@
 #include "ports.h"
 #include "drill.h"
+
+static unsigned int DCShadow;
+
+void Outone(int bit){	//Sets a bit in DCShadow to 1
+	unsigned int mask = 1;
+	if(bit>7 || bit <0){
+		return;
+	} else {
+		mask = mask << bit;
+		DCShadow = DCShadow | mask;	
+	}
+}
+
+void Outzero(int bit){ //Sets a bit in DCShadow to 0
+unsigned int mask = 1;
+	if(bit>7 || bit <0){
+		return;
+	} else {
+		mask = mask << bit;
+		DCShadow = DCShadow & ~mask;	
+	}
+
+
+}
+
+void MotorStart(void);
+void MotorStop(void);
+void DrillDown(void);
+void DrillUp(void);
+int Nste(int);
+int DrillDownTest(void);
+void Alarm(int);
+void DrillHole(void);
+int RefPos(void);
+void DoAuto(void);
+void Outzero(int);
+void Outone(int);

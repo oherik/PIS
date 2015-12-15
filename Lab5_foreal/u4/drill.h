c6e234578ef231fe77b187d0424318bc5b329de8
Erik Öhrn <erik.ohrn@outlook.com>
2015-12-15 10:53:22 +0100

    Added switch case

--- Lab5_foreal/u4/drill.h
+++ Lab5_foreal/u4/drill.h
@@ -3,6 +3,7 @@ void MotorStop(void);
 void DrillDown(void);
 void DrillUp(void);
 int Nstep(int);
+int Step(void);
 int DrillDownTest(void);
 void Alarm(int);
 int DrillHole(void);
--- Lab5_foreal/u4/main.c
+++ Lab5_foreal/u4/main.c
@@ -1,16 +1,44 @@
 #include "clock.h"
 #include "drill.h"
+#include "keyboardML15.h"
 
 void main(void){
 double pattern[21] = { 0, 1, 1, 1, 1, 1, 1, 1, 2, 1, 5, 2, 2, 2, 2, 4, 4, 3, 8, 2, 0xFF}; 
 int* pattern_ptr = pattern;
-time_type time;
-unsigned int time_sec;
+char key;
 
 init_clock();
-while(1){
-	time = get_time();
-	time_sec = time/1000;
-	hold((time_type)1000);
-	}
+
+key = get_key();
+
+switch(key) {
+      case 0 :
+         MotorStart();
+         break;
+      case 1 :
+	MotorStop();
+	break;
+	case 2:
+	DrillDown();
+	break;
+	case 3:
+	DrillUp();
+	break;
+	case 4:
+	Step();
+	break;
+	case 5:
+	DrillHole();
+	break;
+	case 6:
+	RefPos();
+	break;
+	case 7:
+	DoAuto(pattern_ptr);
+	break;    
+   }
+
+
+
+
 }
